72d2084f99fa81ed658dc2e46512ad976fcd86af
Onenantsoa25 <152161654+Onenantsoa25@users.noreply.github.com>
2024-05-17 09:51:57 +0300

    mis a jour sprint1

--- mg/itu/prom/FrontController.java
+++ mg/itu/prom/FrontController.java
@@ -19,24 +19,27 @@ public class FrontController extends HttpServlet {
 
    private String basePackage;
    private List<Class<?>> controllers = new ArrayList<>();
-   private int test = 0;
+   private boolean test;
 
    @Override
    public void init(ServletConfig config) throws ServletException {
       super.init(config);
-      test += 1;
-      basePackage = config.getInitParameter("base-package");
-      try {
-         controllers = scanClasses(basePackage);
-      } catch (Exception e) {
-         throw new ServletException("Erreur lors du scan des classes", e);
-      }
+        basePackage = config.getInitParameter("base-package");
+        test = false;
    }
 
    public FrontController() {
    }
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if(test == false){
+            try {
+                controllers = ScannerClass.scanClasses(basePackage);
+                test = true;
+            } catch (Exception e) {
+                throw new ServletException("Erreur lors du scan des classes", e);
+            }
+        }
         StringBuffer url = request.getRequestURL();
         System.out.println(url);
         response.setContentType("text/plain");
@@ -63,40 +66,4 @@ public class FrontController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    public static List<Class<?>> scanClasses(String packageName) throws Exception {
-
-        List<Class<?>> classes = new ArrayList<>();
-        String path = packageName.replace('.', '/');
-    
-        URL url = Thread.currentThread().getContextClassLoader().getResource(path);
-        if (url == null) {
-            throw new Exception("Package :" + packageName + "nom trouve");
-        }
-    
-        File directory = new File(url.toURI());
-        File[] files = directory.listFiles();
-    
-        for (File file : files) {
-            String fileName = file.getName();
-            //~ System.out.println("File : " + fileName);
-
-            if (fileName.endsWith(".class")) {
-                String className = packageName + '.' + fileName.substring(0, fileName.length() - 6);
-    
-                try {
-                    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-                    Class<?> loadedClass = classLoader.loadClass(className);
-                    if (loadedClass.getAnnotation(Controleur.class) != null) {
-                        classes.add(loadedClass);   
-                    }
-                } 
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return classes;
-    }
-
 }
