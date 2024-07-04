ec6da98131b729b2502795bda58c3ebaa3a2721d
Onenantsoa25 <152161654+Onenantsoa25@users.noreply.github.com>
2024-07-04 11:04:24 +0300

    Sprint8_2
    Correction

--- mg/itu/prom/FrontController.java
+++ mg/itu/prom/FrontController.java
@@ -11,7 +11,7 @@ import jakarta.servlet.RequestDispatcher;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public class FrontController extends HttpServlet {
 
