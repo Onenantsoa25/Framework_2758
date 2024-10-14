00d595ae7501044a445b095d4fda1d2e29e43f38
Onenantsoa25 <152161654+Onenantsoa25@users.noreply.github.com>
2024-10-14 21:24:29 +0300

    Sprint10_2758
    get, post, url, verbMethod

--- mg/itu/prom/Url.java
+++ mg/itu/prom/Url.java
@@ -1,11 +1,11 @@
 package mg.itu.prom;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)  // Cette annotation sera utilisée sur des méthodes
-public @interface Get {
+public class Url {
+    String value() default "/";
 }
---
+++ mg/itu/prom/VerbMethod.java
@@ -0,0 +1,19 @@
+package mg.itu.prom;
+
+public class VerbMethod {
+    String verb;
+    String method;
+
+    public void setVerb(String verb){
+        this.verb = verb;
+    }
+
+    public void setMethod(String method){
+        this.method = method;
+    }
+
+    public String getVerb() { return verb; }
+
+    public String gertMethod() { return method; }
+
+}
